e102ae4d989d3eee8b73fa4935e14d50eb22d7f6
Sree Balasubramaniyan <sreebalasubramaniyan682@gmail.com>
2025-08-09 11:09:35 +0530

    Time: 988 ms (5%), Space: 12.7 MB (92.32%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,20 +1,14 @@
 class Solution(object):
     def characterReplacement(self, S, k):  
-        maxLen = 0 ;n = len(S);fliped = 0  
-        for target in set(S) :
-            i = j = 0 
-            while  j <  n : 
-                cur = target
-                if cur != S[j] :
-                    fliped += 1
-                while fliped > k:
-                    if S[i] != cur :
-                        fliped -= 1
-                    i += 1
-                maxLen = max(maxLen , j - i + 1) 
-                j += 1          
-            return maxLen 
 
+          # Hashmap approach
+        # brut force : O(n^2)
+        # 1) generate all substring and updated maxLen if that substring satisy the condition or break 
+        # 2) we want to find the maximum freq element in our current susbtring(parent) max(hashMap.values()) and remaining elements(Len(susbstrig) - maxFreq) and if the change <= k then we can update our maxLen or else break it and go to next susbtring
+
+        # from that brut force we can reduce n ^ 2 to O(n) by our treditionnal method 
+            # valid Grow, invalid Stop, shrink one step(left)
+            # Change maxLen only when it's valid 
         hashMap = {}
         i = j = maxLen = maxFreq = 0
         while j < len(S) :
@@ -32,23 +26,20 @@ class Solution(object):
             j += 1
             print(maxLen)
         return maxLen
-
-class Solution(object):
-    def characterReplacement(self, S, k):
-        maxLen = 0
-        n = len(S)
-
-        # Try making the string all of each possible letter
-        for target in set(S):
-            i = j = 0
-            flips = 0
-            while j < n:
-                if S[j] != target:
-                    flips += 1
-                while flips > k:
-                    if S[i] != target:
-                        flips -= 1
+        
+        # My approach 
+        maxLen = 0 ;n = len(S);fliped = 0  
+        for target in set(S) :
+            i = j = 0 
+            while  j <  n : 
+                cur = target
+                if cur != S[j] :
+                    fliped += 1
+                while fliped > k:
+                    if S[i] != cur :
+                        fliped -= 1
                     i += 1
-                maxLen = max(maxLen, j - i + 1)
-                j += 1
-        return maxLen
+                maxLen = max(maxLen , j - i + 1) 
+                j += 1          
+            return maxLen 
+  
