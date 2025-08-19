17a87c865fd25d0efe25796effa2902774f19d7b
Sree Balasubramaniyan <sreebalasubramaniyan682@gmail.com>
2025-08-20 01:53:23 +0530

    Time: 46 ms (78.77%), Space: 13.4 MB (64.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,20 +1,20 @@
 class Solution(object):
     def findAnagrams(self, s, p):
-        hashMap1 = {}
-        for i in p: 
-            hashMap1[i] = hashMap1.get(i,0) + 1
         if  len(p)  > len(s) : return []
-        n = len(p) ; m = len(s) ; res = [] ; hashMap = {}
-        for i in range(n) : hashMap[s[i]] = hashMap.get(s[i],0) + 1
-        print(hashMap1, hashMap)
-        for i in range(n,m) :
-            print("X : " ,  hashMap)
-            if hashMap == hashMap1 :
-                res.append(i-n)
+
+        n = len(p) ; m = len(s) ; res = []  
+        hashMap = {} ;hashMap1 = {}
+
+        for i in p : hashMap1[i] = hashMap1.get(i,0) + 1
+        for i in range(n-1) : hashMap[s[i]] = hashMap.get(s[i],0) + 1
+
+        for i in range(n-1,m) :
             hashMap[s[i]] = hashMap.get(s[i],0) + 1
-            hashMap[s[i-n]] -= 1
-            if hashMap[s[i-n]] == 0 :
-                del hashMap[s[i-n]]
-        
-        if hashMap == hashMap1 : res.append(m-n)
+            if hashMap == hashMap1 :
+                res.append(i-n+1)
+            hashMap[s[i-n+1]] -= 1
+            if hashMap[s[i-n+1]] == 0 :
+                del hashMap[s[i-n+1]]
+
+        # if hashMap == hashMap1 : res.append(m-n)
         return res
