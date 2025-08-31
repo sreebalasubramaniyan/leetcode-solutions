d4c78fd7dff4a2a7db3a1e44acdd70a5a07ce95d
Sree Balasubramaniyan <sreebalasubramaniyan682@gmail.com>
2025-08-31 18:39:12 +0530

    Time: 100 ms (12.73%), Space: 21.2 MB (95.45%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -12,18 +12,26 @@ class Solution(object):
         # return max(res,1)
         if not nums : return 0
         nums.sort() ; maxLen = float('-inf')
-        i = 0 
-        while i < len(nums) :
-            curLen = 1
-            while i + 1 < len(nums) and nums[i] + 1 == nums[i+1] :
-                curLen += 1 ; 
-                i += 1
-                while i + 1 < len(nums) and nums[i] == nums[i+1] :
-                    i += 1
-            i += 1
-            
+        # i = 0 
+        # while i < le(nums) :
+        #     curLen = 1
+        #     while i + 1 < len(nums) and nums[i] + 1 == nums[i+1] :
+        #         curLen += 1 ; 
+        #         i += 1
+        #         while i + 1 < len(nums) and nums[i] == nums[i+1] :
+        #             i += 1
+        #     i += 1
+        prev = nums[0] ; curLen = 1 ; maxLen = float('-inf')
+        if len(nums) == 1 : return 1
+        for i in range(1,len(nums)) :
+            if nums[i] == prev : continue
+            if prev + 1 == nums[i] :
+                curLen += 1
+            else :
+                curLen = 1
+            prev = nums[i]
             maxLen = max(maxLen,curLen)
-        return maxLen
+        return max(maxLen,curLen)
 
 
 
