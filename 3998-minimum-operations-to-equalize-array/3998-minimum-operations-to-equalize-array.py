ca4a07b3b4883572a4a51c3ccf02f8ff3d568b6c
Sree Balasubramaniyan <sreebalasubramaniyan682@gmail.com>
2025-09-07 15:56:35 +0530

    Time: 0 ms (100%), Space: 12.4 MB (100%) - LeetHub

--- 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.py
+++ 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.py
@@ -6,4 +6,12 @@ class Solution(object):
     # 0 : array already has same elements(no need of change)
     # 1 : and operation of the entire array (only once)
 
+    # nums = [a,b,c,...,x,y,z] 
+    # we always choosing the entire array as a subarray
+    # so at the end we get any answer that does not matter
+    # and simply we replace the all elements with that ans
+    # so no operation will be alwasy will be 1
+
+    # if the all elements are already same means the operation will be 0(no need) 
+
         return 0 if len(set(nums)) == 1 else 1
