35433d6454535aa9d9d33e874b61b68f92c5c33f
Sree Balasubramaniyan <sreebalasubramaniyan682@gmail.com>
2025-09-10 21:58:01 +0530

    Time: 0 ms (100%), Space: 12.6 MB (10.42%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
@@ -10,19 +10,19 @@ class Solution(object):
 
         """
 
-        # S = "" 
-        # for i in s :
-        #     if i == "(" or i==")" :
-        #         S += i
-        # if not S :  return 0
-        stack = []
+        # here insead of using stack we can just go with the counter
+
+        # stack = []
+        count = 0
         ans = float('-inf')
         for i in s :
             if i == "(":
-                stack.append(i)
+                # stack.append(i)
+                count += 1
             elif i == ")" :
-                cur = len(stack)
-                ans = max(ans,cur)
-                stack.pop()
+                # cur = len(stack)
+                ans = max(ans,count)
+                # stack.pop()
+                count-=1
         if ans == float('-inf') : return 0
         return ans
