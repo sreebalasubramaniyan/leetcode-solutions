2ce092adede1b0a4ff2ed12771cf1f4d4d198595
Sree Balasubramaniyan <sreebalasubramaniyan682@gmail.com>
2025-12-10 22:05:04 +0530

    Time: 11 ms (42.71%), Space: 13.1 MB (69.21%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        from start  
+        ans = 1  
+        2 3 0 1 
+
+        reversed engineering
+        """
+
+        res = 0
+        l = r = 0 
+        while r < len(nums)-1:
+            farthest = 0
+            for i in range(l,r+1):
+                farthest = max(farthest,i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
+
+            
+                 
